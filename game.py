9fefd2dcc39378392a8cdb7ed108c5140abe2772
Sahan-A-2003 <sahan.asantha2003@gmail.com>
2026-04-22 11:36:15 +0530

    add the gueesing code

---
+++ game.py
@@ -0,0 +1,31 @@
+import random
+
+COLORS = ["R", "G", "B", "Y", "W", "O"]
+TRIES = 10
+CODE_LENGTH = 4
+
+def generate_code():
+  code = []
+  for _ in range(CODE_LENGTH):
+    coloer = random.choice(COLORS)
+    code.append(coloer)
+
+  return code
+
+def guess_code():
+  while True:
+    guess = input("Guess: ").upper().split()
+
+    if len(guess) != CODE_LENGTH:
+      print(f"You must guess {CODE_LENGTH} color.")
+      continue
+    
+    for color in guess:
+      if color not in COLORS:
+        print(f"Invalid color: {color}. Try again.")
+        break
+    else:
+      break
+
+  return guess
+
